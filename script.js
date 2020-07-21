2b791041ac186460070521b690e87895ea90e66d
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-07-20 19:10:01 -0700

    API call for IP

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("#login").on("click", function () {
-
+        event.preventDefault()
         validateForm();
 
     })
@@ -13,16 +13,22 @@ $(document).ready(function () {
         inputVal.push(username);
         inputVal.push(password);
 
-        if (inputVal[0] == ""){
+        if (inputVal[0] == "") {
             $('#username').after('<div class="error"> Please enter your username </div>');
-        } 
-        if (inputVal[1]){
+        }
+        if (inputVal[1]) {
             $('#password').after('<span class="error"> Please enter a password </span>');
         }
 
 
-        // if (username === "" || password === "") {
-
+        // if (username !== "" || password !== "") {
+        var queryURL = "http://ip-api.com/json/"
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (res) {
+            var ip = res.query
+        });
         // }
     }
 });
