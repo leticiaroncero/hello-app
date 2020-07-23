2694682053edd6cdc77e082408e5f0e90e59e362
Shipra <drshipradyundi@gmail.com>
2020-07-22 20:59:55 -0700

    fixed an issue with greeting

--- script.js
+++ script.js
@@ -24,13 +24,15 @@ $(document).ready(function () {
                 url: "https://fourtonfish.com/hellosalut/?ip=" + ip,
                 method: "GET"
             }).then(function (res) {
-                var greeting = res.hello
-                $("#greeting").text(greeting).html("<span>").text(username + " you have successfully logged in!");
+                var greeting = res.hello;
+                //var firstPiece = $("#greeting").text(greeting)
+                $("#greeting").html("<span>").text(greeting + " " + username + " you have successfully logged in!");
                 var button = $('<button id="logout">Logout</button>');
                 $("#logoutBtn").append(button);
                 $("#logout").on("click", function (event) {
                     var username = $("#username").val().trim();
                     $("form")[0].reset();
+                   
                     $("#greeting").html("<span>").text( "Have a great day " + username);
                      
                  });
